f7d06bbc970f7b70f56c5734b8bc25c1b0d62ba6
daniel-chionne <160922948+Chionne27@users.noreply.github.com>
2024-09-03 10:56:58 +0200

    [FIX] user page updated and content dashborad

--- client/src/components/codeUsage.jsx
+++ client/src/components/codeUsage.jsx
@@ -3,30 +3,30 @@ import axios from 'axios';
 import { useRecoveryContext } from '../provider/provider';
 
 const CodeUsage = () => {
-    const [copiedCodes, setCopiedCodes] = useState({});
-    const [codici, setCodici] = useState([]);
+  const [copiedCodes, setCopiedCodes] = useState({});
+  const [codici, setCodici] = useState([]);
 
-    const { codeTrigger } = useRecoveryContext();
+  const { codeTrigger } = useRecoveryContext();
 
   useEffect(() => {
 
     const fetchFromPublished = async () => {
 
-        try{
-            const response = await axios.get('http://localhost:8080/api/fetch-published-codes', 
-                {
-                  headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                  }
-                }
-              );
-              if (response.status === 200) {
-                setCodici(response.data.codes);
-              }
-        } catch (error) {
-          console.log(error);
+      try {
+        const response = await axios.get('http://localhost:8080/api/fetch-published-codes',
+          {
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+          }
+        );
+        if (response.status === 200) {
+          setCodici(response.data.codes);
         }
-      
+      } catch (error) {
+        console.log(error);
+      }
+
     };
     fetchFromPublished();
   }, [codeTrigger]);
@@ -42,24 +42,25 @@ const CodeUsage = () => {
 
   return (
     <div className="w-full lg:w-1/2 bg-yellow-500 p-4 rounded-lg overflow-y-auto flex-1">
-        <h2 className="text-2xl font-bold mb-4 text-center lg:text-left">Codici sconto disponibili</h2>
-        {codici.map((codice, index) => (
-            <div
-                key={index} // Aggiungi una chiave unica per migliorare le prestazioni di React
-                className="mb-4 flex flex-row items-center lg:items-start justify-between bg-white p-4 rounded-lg shadow tranform transition duration-300 hover:scale-[1.02] hover:shadow-lg" 
-            >
-                <div className="flex flex-col items-start space-y-2 lg:space-y-0 w-full lg:w-auto">
-                    <span className="text-lg font-mono text-red-500 flex-shrink-0">{codice.code}</span>
-                    <span className="text-lg font-mono text-gray-700">Utilizzo: {codice.used_by}</span>
-                </div>
-                <button
-                    onClick={() => handleCopy(codice.code)}
-                    className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-300 my-auto"
-                >
-                    {copiedCodes[codice.code] ? 'Copiato!' : 'Copia'}
-                </button>
-            </div>
-        ))}
+      <h2 className="text-2xl font-bold mb-4 text-center lg:text-left">Codici sconto disponibili</h2>
+      {codici.length === 0 && <p>Nessun codice disponibile</p>}
+      {codici.map((codice, index) => (
+        <div
+          key={index} // Aggiungi una chiave unica per migliorare le prestazioni di React
+          className="mb-4 flex flex-row items-center lg:items-start justify-between bg-white p-4 rounded-lg shadow tranform transition duration-300 hover:scale-[1.02] hover:shadow-lg"
+        >
+          <div className="flex flex-col items-start space-y-2 lg:space-y-0 w-full lg:w-auto">
+            <span className="text-lg font-mono text-red-500 flex-shrink-0">{codice.code}</span>
+            <span className="text-lg font-mono text-gray-700">Utilizzo: {codice.used_by}</span>
+          </div>
+          <button
+            onClick={() => handleCopy(codice.code)}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-300 my-auto"
+          >
+            {copiedCodes[codice.code] ? 'Copiato!' : 'Copia'}
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
--- client/src/components/content_dashboard.jsx
+++ client/src/components/content_dashboard.jsx
@@ -172,7 +172,7 @@ const Dashboard = () => {
 
       if (response.status === 200) {
         const updatedItem = response.data.tuple;
-        console.log("ciao",updatedItem);
+        console.log("ciao", updatedItem);
         if (type === "news") {
           setNews((prevNews) =>
             prevNews.map((news) => (news.id === id ? updatedItem : news))
@@ -228,13 +228,167 @@ const Dashboard = () => {
         {messageConfirm}
       </ConfirmPopUp>
 
-      <div className="flex flex-col">
+      <div className="flex flex-wrap justify-center items-center gap-4">
         <button
-          className="bg-[#a3c4b5] text-[#2d7044] px-4 py-2 rounded mb-4 transition-transform transform hover:scale-[1.03] hover:bg-[#8db3a2] shadow-sm hover:shadow-md"
+          className="w-[300px] h-[100px] mb-4 rounded-lg transition-transform transform hover:scale-105 shadow-sm hover:shadow-md text-white flex justify-center items-center gap-2"
+          style={{ backgroundColor: "#2d7044" }}
           onClick={() => setShowNews(!showNews)}
         >
-          {showNews ? "Nascondi News" : "Mostra News"}
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+          >
+            <path
+              d="M5 21h12a4 4 0 0 0 4-4V5a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v13c0 1.657-.343 3-2 3Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M3 10a2 2 0 0 1 2-2h2v10.5c0 1.38-.62 2.5-2 2.5s-2-1.12-2-2.5V10Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <circle cx="12" cy="8" r="1" fill="currentColor" />
+            <path d="M11 14h6m-6 3h3" stroke="currentColor" strokeWidth="2" />
+          </svg>
+          <span>{showNews ? "Nascondi News" : "Mostra News"}</span>
         </button>
+
+        <button
+          className="w-[300px] h-[100px] mb-4 rounded-lg transition-transform transform hover:scale-105 shadow-sm hover:shadow-md text-white flex justify-center items-center gap-2"
+          style={{ backgroundColor: "#2d7044" }}
+          onClick={() => setShowProducts(!showProducts)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+          >
+            <g fill="currentColor">
+              <path
+                d="M1 4a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H7v-.354A4.01 4.01 0 0 0 7.465 11H13a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1.535a4.018 4.018 0 0 0-1 .82V4Zm5.646 2a3.984 3.984 0 0 0-2.275-.983A.5.5 0 0 1 4.5 5h7a.5.5 0 0 1 0 1H6.646ZM8.5 8a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3ZM7 9a3 3 0 1 1-6 0a3 3 0 0 1 6 0Zm-5 3.443V14.5a.495.495 0 0 0 .264.441a.5.5 0 0 0 .513-.026L4 14.1l1.223.816a.498.498 0 0 0 .706-.16a.505.505 0 0 0 .071-.257v-2.056a3.959 3.959 0 0 1-2 .556a3.959 3.959 0 0 1-2-.556Z"
+              />
+            </g>
+          </svg>
+          <span>{showProducts ? "Nascondi Certificazioni" : "Mostra Certificazioni"}</span>
+        </button>
+
+        <button
+          className="w-[300px] h-[100px] mb-4 rounded-lg transition-transform transform hover:scale-105 shadow-sm hover:shadow-md text-white flex justify-center items-center gap-2"
+          style={{ backgroundColor: "#2d7044" }}
+          onClick={() => setShowMessages(!showMessages)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+          >
+            <g fill="#feffff">
+              <g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2">
+                <path strokeLinejoin="round" d="M14 19c3.771 0 5.657 0 6.828-1.172C22 16.657 22 14.771 22 11c0-3.771 0-5.657-1.172-6.828C19.657 3 17.771 3 14 3h-4C6.229 3 4.343 3 3.172 4.172C2 5.343 2 7.229 2 11c0 3.771 0 5.657 1.172 6.828c.653.654 1.528.943 2.828 1.07" />
+                <path d="M12 11v.01M8 11v.01m8-.01v.01M14 19c-1.236 0-2.598.5-3.841 1.145c-1.998 1.037-2.997 1.556-3.489 1.225c-.492-.33-.399-1.355-.212-3.404L6.5 17.5" />
+              </g>
+            </g>
+          </svg>
+          <span>{showMessages ? "Nascondi Messaggi" : "Mostra Messaggi"}</span>
+        </button>
+
+        <button
+          className="w-[300px] h-[100px] mb-4 rounded-lg transition-transform transform hover:scale-105 shadow-sm hover:shadow-md text-white flex justify-center items-center gap-2"
+          style={{ backgroundColor: "#2d7044" }}
+          onClick={() => setShowOrders(!showOrders)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+          >
+            <g fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2">
+              <path d="M5 7h13.79a2 2 0 0 1 1.99 2.199l-.6 6A2 2 0 0 1 18.19 17H8.64a2 2 0 0 1-1.962-1.608L5 7Z" />
+              <path strokeLinecap="round" d="m5 7l-.81-3.243A1 1 0 0 0 3.22 3H2m6 18h2m6 0h2" />
+            </g>
+          </svg>
+          <span>{showOrders ? "Nascondi Ordini utenti" : "Mostra Ordini utenti"}</span>
+        </button>
+
+        <button
+          className="w-[300px] h-[100px] mb-4 rounded-lg transition-transform transform hover:scale-105 shadow-sm hover:shadow-md text-white flex justify-center items-center gap-2"
+          style={{ backgroundColor: "#2d7044" }}
+          onClick={() => setShowPromocodes(!showPromocodes)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 480 496"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+          >
+            <path
+              fill="currentColor"
+              d="M91 347v85l85-21l64 64l64-64l85 21v-85l86-22l-43-85l43-85l-86-22V48l-85 21l-64-64l-64 64l-85-21v85L5 155l43 85l-43 85zm-5-126l-19-39l34-8q15-3 23.5-14.5T133 133v-30l32 9h11q17 0 30-13l34-34l34 34q13 13 30 13q6 0 11-2l32-7v30q0 15 8.5 26.5T379 174l34 8l-19 39q-10 19 0 38l19 39l-34 8q-15 3-23.5 14.5T347 347v30l-32-9h-11q-17 0-30 13l-34 34l-34-34q-13-13-30-13q-6 0-11 2l-32 7v-30q0-15-8.5-26.5T101 306l-34-8l19-39q10-19 0-38zm133 92l100-101q14-14 0-30q-15-13-30 0l-70 71l-28-28q-15-13-30 0q-13 15 0 30z"
+            />
+          </svg>
+          <span>{showPromocodes ? "Nascondi Codici Promo" : "Mostra Codici Promo"}</span>
+        </button>
+
+        <button
+          className="w-[300px] h-[100px] mb-4 rounded-lg transition-transform transform hover:scale-105 shadow-sm hover:shadow-md text-white flex justify-center items-center gap-2"
+          style={{ backgroundColor: "#2d7044" }}
+          onClick={() => setShowForms(!showForms)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+          >
+            <g fill="currentColor">
+              <path
+                d="M21 12H7a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1ZM8 10h12V7.94H8Z"
+              />
+              <path
+                d="M21 14.08H7a1 1 0 0 0-1 1V19a1 1 0 0 0 1 1h11.36L22 16.3v-1.22a1 1 0 0 0-1-1ZM20 18H8v-2h12Z"
+              />
+              <path
+                d="M11.06 31.51v-.06l.32-1.39H4V4h20v10.25l2-1.89V3a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v28a1 1 0 0 0 1 1h8a3.44 3.44 0 0 1 .06-.49Z"
+              />
+              <path
+                d="m22 19.17l-.78.79a1 1 0 0 0 .78-.79Z"
+              />
+              <path
+                d="M6 26.94a1 1 0 0 0 1 1h4.84l.3-1.3l.13-.55v-.05H8V24h6.34l2-2H7a1 1 0 0 0-1 1Z"
+              />
+              <path
+                d="m33.49 16.67l-3.37-3.37a1.61 1.61 0 0 0-2.28 0L14.13 27.09L13 31.9a1.61 1.61 0 0 0 1.26 1.9a1.55 1.55 0 0 0 .31 0a1.15 1.15 0 0 0 .37 0l4.85-1.07L33.49 19a1.6 1.6 0 0 0 0-2.27ZM18.77 30.91l-3.66.81l.89-3.63L26.28 17.7l2.82 2.82Zm11.46-11.52l-2.82-2.82L29 15l2.84 2.84Z"
+              />
+            </g>
+          </svg>
+          <span>{showForms ? "Nascondi Forms" : "Mostra Forms"}</span>
+        </button>
+      </div>
+
+      <div className="flex flex-col">
+
         {showNews && (
           <div className="w-full mb-8">
             <h2 className="text-2xl font-bold mb-4">
@@ -330,7 +484,7 @@ const Dashboard = () => {
                             onChange={handleQuillChange}
                             className="mb-4"
                           />*/}
-                          <TextEditor value={itemToEdit.content || ""} onChange={handleQuillChange}/>
+                          <TextEditor value={itemToEdit.content || ""} onChange={handleQuillChange} />
                         </label>
                         <div className="flex justify-center gap-3">
                           <button
@@ -367,12 +521,6 @@ const Dashboard = () => {
           </div>
         )}
 
-        <button
-          className="bg-[#a3c4b5] text-[#2d7044] px-4 py-2 rounded mb-4 transition-transform transform hover:scale-[1.03] hover:bg-[#8db3a2] shadow-sm hover:shadow-md"
-          onClick={() => setShowProducts(!showProducts)}
-        >
-          {showProducts ? "Nascondi Certificazioni" : "Mostra Certificazioni"}
-        </button>
         {showProducts && (
           <div className="w-full">
             <h2 className="text-2xl font-bold mb-4">
@@ -583,40 +731,13 @@ const Dashboard = () => {
           </div>
         )}
       </div>
-      <button
-        className="bg-[#a3c4b5] text-[#2d7044] px-4 py-2 rounded mb-4 transition-transform transform hover:scale-[1.03] hover:bg-[#8db3a2] shadow-sm hover:shadow-md"
-        onClick={() => setShowMessages(!showMessages)}
-      >
-        {showMessages ? "Nascondi Messaggi" : "Mostra Messaggi"}
-      </button>
+
       {showMessages && <MessagesDashboard />}
-
-      <button
-        className="bg-[#a3c4b5] text-[#2d7044] px-4 py-2 rounded mb-4 transition-transform transform hover:scale-[1.03] hover:bg-[#8db3a2] shadow-sm hover:shadow-md"
-        onClick={() => setShowOrders(!showOrders)}
-      >
-        {showOrders ? "Nascondi Ordini utenti" : "Mostra Ordini utenti"}
-      </button>
       {showOrders && <AllOrders />}
-
-      <button
-        className="bg-[#a3c4b5] text-[#2d7044] px-4 py-2 rounded mb-4 transition-transform transform hover:scale-[1.03] hover:bg-[#8db3a2] shadow-sm hover:shadow-md"
-        onClick={() => setShowPromocodes(!showPromocodes)}
-      >
-        {showPromocodes ? "Nascondi Codici Promozionali" : "Mostra Codici Promozionali"}
-      </button>
       {showPromocodes && <PromoCodes />}
-
-      <button
-        className="bg-[#a3c4b5] text-[#2d7044] px-4 py-2 rounded mb-4 transition-transform transform hover:scale-[1.03] hover:bg-[#8db3a2] shadow-sm hover:shadow-md"
-        onClick={() => setShowForms(!showForms)}
-      >
-        {showForms ? "Nascondi Forms" : "Mostra Forms"}
-      </button>
       {showForms && <NewsForm />}
       {showForms && <ProductsForm />}
       {showForms && <PromoCodeForm />}
-
 
     </div>
   );
