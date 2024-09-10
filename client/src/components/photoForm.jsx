9470d1c3dd752d530e80faeaa3b5ce5df4398b7e
daniel-chionne <160922948+Chionne27@users.noreply.github.com>
2024-09-10 23:01:10 +0200

    [FIX] building system, modify plants solars and photovoltaics

--- client/src/components/photoForm.jsx
+++ client/src/components/photoForm.jsx
@@ -4,8 +4,8 @@ import MessagePopUp from "./messagePopUp";
 import { MutatingDots } from "react-loader-spinner";
 import { useRecoveryContext } from "../provider/provider";
 
-function PhotoForm() {
-    const [power, setPower] = useState("");
+function PhotoForm({ photo = 'empty', isEdit, onButtonClick = "empty" }) {
+    const [power, setPower] = useState(photo.power || 0);
     const [isLoading, setIsLoading] = useState(false);
     const [buttonPopup, setButtonPopup] = useState(false);
     const [messagePopup, setMessagePopup] = useState('');
@@ -27,10 +27,50 @@ function PhotoForm() {
         }
     };
 
+    const handleUpdatePhoto = async () => {
+        const token = localStorage.getItem("token");
+        const id = buildingID;
+
+        const formData = {
+            power: parseFloat(power),
+            photoScore: calculatePowerScore(power)  // Calcolo e aggiunta del punteggio di ecosostenibilità
+        };
+
+        try {
+            const response = await axios.put(`http://localhost:8080/api/buildings/${id}/update-photovoltaic/${photo.id}`, formData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.status === 200) {
+                setMessagePopup(response.data.msg);
+                setButtonPopup(true);
+                setIsLoading(false);
+                setPower(""); // Clear form field
+
+                triggerRefresh();
+
+            } else if (response.status === 400) {
+                setMessagePopup(response.data.msg);
+                setButtonPopup(true);
+                setIsLoading(false);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setIsLoading(true);
+
+        if (isEdit) {
+            handleUpdatePhoto();
+            return;
+        }
 
         const token = localStorage.getItem("token");
         const id = buildingID;
@@ -76,7 +116,7 @@ function PhotoForm() {
                 {messagePopup}
             </MessagePopUp>
             <div className="w-[98.5%] mx-auto my-10 md:m-4 rounded-2xl font-arial text-xl px-10 py-6 border border-gray-300 shadow-xl bg-[#f6f3f3] mb-4">
-                <h2 className="text-2xl font-bold text-center mb-6">Inserisci un nuovo impianto fotovoltaico</h2>
+                <h2 className="text-2xl font-bold text-center mb-6">{isEdit ? "Modifica fotovoltaico" : "Aggiungi un nuovo fotovoltaico"}</h2>
                 <form onSubmit={handleSubmit} className="flex flex-col">
 
                     <div className="mb-6">
@@ -93,29 +133,37 @@ function PhotoForm() {
                         </label>
                     </div>
 
-                    <button
-                        type="submit"
-                        className="mt-7 mx-auto font-arial text-xl w-[30%] md:text-2xl md:w-[30%] lg:text-2xl lg:w-[20%] p-1 bg-[#2d7044] text-white rounded-lg border-2 border-transparent hover:border-[#2d7044] transition-colors duration-300 ease-in-out hover:bg-white hover:text-[#2d7044]"
-                        disabled={isLoading}
-                    >
+                    <div className="flex justify-center">
                         {isLoading ? (
-                            <div className="flex justify-center">
+                            <div className="flex justify-center items-center mt-5">
                                 <MutatingDots
                                     height="100"
                                     width="100"
-                                    color="#ffffff"
-                                    secondaryColor="#ffffff"
-                                    radius="12.5"
+                                    color="#2d7044"
+                                    secondaryColor='#2d7044'
+                                    radius='12.5'
                                     ariaLabel="mutating-dots-loading"
-                                    wrapperStyle={{}}
-                                    wrapperClass=""
                                     visible={true}
                                 />
                             </div>
                         ) : (
-                            "Carica"
+                            <div className=" w-full flex justify-center items-center mt-5 gap-3">
+                                <button
+                                    type="submit"
+                                    className="mt-7 font-arial text-xl w-[30%] md:text-2xl md:w-[30%] lg:text-2xl lg:w-[20%] p-1 bg-[#2d7044] text-white rounded-lg border-2 border-transparent hover:border-[#2d7044] transition-colors duration-300 ease-in-out hover:bg-white hover:text-[#2d7044]"
+                                >
+                                    Carica
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={onButtonClick}
+                                    className="mt-7 font-arial text-xl w-[30%] md:text-2xl md:w-[30%] lg:text-2xl lg:w-[20%] p-1 bg-gray-500 text-white rounded-lg border-2 border-transparent hover:border-gray-500 transition-colors duration-300 ease-in-out hover:bg-white hover:text-gray-500"
+                                >
+                                    Annulla
+                                </button>
+                            </div>
                         )}
-                    </button>
+                    </div>
                 </form>
             </div>
         </div>
--- client/src/components/solarForm.jsx
+++ client/src/components/solarForm.jsx
@@ -4,8 +4,8 @@ import MessagePopUp from "./messagePopUp";
 import { MutatingDots } from "react-loader-spinner";
 import { useRecoveryContext } from "../provider/provider";
 
-function SolarForm() {
-    const [installedArea, setInstalledArea] = useState("");
+function SolarForm({ solar = 'empty', isEdit, onButtonClick = "empty" }) {
+    const [installedArea, setInstalledArea] = useState(solar.installed_area || "");
     const [isLoading, setIsLoading] = useState(false);
     const [buttonPopup, setButtonPopup] = useState(false);
     const [messagePopup, setMessagePopup] = useState('');
@@ -28,9 +28,56 @@ function SolarForm() {
     };
 
 
+    const handleUpdateSolar = async () => {
+        const token = localStorage.getItem("token");
+        const id = buildingID;
+
+        const area = parseFloat(installedArea);
+        const formData = {
+            installedArea: area,
+            solarScore: calculateEcoScore(area)  // Calcolo e aggiunta del punteggio di ecosostenibilità
+        };
+
+        try {
+            const response = await axios.put(`http://localhost:8080/api/buildings/${id}/update/solar/${solar.id}`, formData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.status === 200) {
+                setMessagePopup(response.data.msg);
+                setButtonPopup(true);
+                setIsLoading(false);
+
+                triggerRefresh();
+
+            } else if (response.status === 400) {
+                setMessagePopup(response.data.msg);
+                setButtonPopup(true);
+                setIsLoading(false);
+            } else {
+                console.log('Error:', response.data.msg);
+                setMessagePopup(response.data.msg);
+                setButtonPopup(true);
+                setIsLoading(false);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         setIsLoading(true);
+
+        if (isEdit) {
+            handleUpdateSolar();
+            return;
+        }
 
         const token = localStorage.getItem("token");
         const id = buildingID;
@@ -69,15 +116,19 @@ function SolarForm() {
         }
     };
 
+    const cancelEdit = () => {
+
+    };
+
     const handleInstalledAreaChange = (e) => setInstalledArea(e.target.value);
 
     return (
-        <div className="w-full mx-2">
+        <div className="w-full mx-2 flex justify-center">
             <MessagePopUp trigger={buttonPopup} setTrigger={setButtonPopup}>
                 {messagePopup}
             </MessagePopUp>
             <div className="w-[98.5%] mx-auto my-10 md:m-4 rounded-2xl font-arial text-xl px-10 py-6 border border-gray-300 shadow-xl bg-[#f6f3f3] mb-4">
-                <h2 className="text-2xl font-bold text-center mb-6">Inserisci un nuovo impianto solare termico</h2>
+                <h2 className="text-2xl font-bold text-center mb-6">{isEdit ? 'Modifica impianto solare termico' : 'Aggiungi impianto solare termico'}</h2>
                 <form onSubmit={handleSubmit} className="flex flex-col">
 
                     <div className="mb-6">
@@ -94,29 +145,35 @@ function SolarForm() {
                         </label>
                     </div>
 
-                    <button
-                        type="submit"
-                        className="mt-7 mx-auto font-arial text-xl w-[30%] md:text-2xl md:w-[30%] lg:text-2xl lg:w-[20%] p-1 bg-[#2d7044] text-white rounded-lg border-2 border-transparent hover:border-[#2d7044] transition-colors duration-300 ease-in-out hover:bg-white hover:text-[#2d7044]"
-                        disabled={isLoading}
-                    >
-                        {isLoading ? (
-                            <div className="flex justify-center">
-                                <MutatingDots
-                                    height="100"
-                                    width="100"
-                                    color="#ffffff"
-                                    secondaryColor="#ffffff"
-                                    radius="12.5"
-                                    ariaLabel="mutating-dots-loading"
-                                    wrapperStyle={{}}
-                                    wrapperClass=""
-                                    visible={true}
-                                />
-                            </div>
-                        ) : (
-                            "Carica"
-                        )}
-                    </button>
+                    {isLoading ? (
+                        <div className="flex justify-center items-center mt-5">
+                            <MutatingDots
+                                height="100"
+                                width="100"
+                                color="#2d7044"
+                                secondaryColor='#2d7044'
+                                radius='12.5'
+                                ariaLabel="mutating-dots-loading"
+                                visible={true}
+                            />
+                        </div>
+                    ) : (
+                        <div className="flex justify-center items-center mt-5 gap-3">
+                            <button
+                                type="submit"
+                                className="mt-7 font-arial text-xl w-[30%] md:text-2xl md:w-[30%] lg:text-2xl lg:w-[20%] p-1 bg-[#2d7044] text-white rounded-lg border-2 border-transparent hover:border-[#2d7044] transition-colors duration-300 ease-in-out hover:bg-white hover:text-[#2d7044]"
+                            >
+                                Carica
+                            </button>
+                            <button
+                                type="button"
+                                onClick={onButtonClick}
+                                className="mt-7 font-arial text-xl w-[30%] md:text-2xl md:w-[30%] lg:text-2xl lg:w-[20%] p-1 bg-gray-500 text-white rounded-lg border-2 border-transparent hover:border-gray-500 transition-colors duration-300 ease-in-out hover:bg-white hover:text-gray-500"
+                            >
+                                Annulla
+                            </button>
+                        </div>
+                    )}
                 </form>
             </div>
         </div>
